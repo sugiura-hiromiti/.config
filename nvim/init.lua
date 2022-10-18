da5506e97fb83133d2e5d1b11084712985bf8059
sugiura-hiromichi <sugiura130418@icloud.com>
2022-10-18 15:10:27 +0900

    mod

--- nvim/init.lua
+++ nvim/init.lua
@@ -60,7 +60,7 @@ require 'packer'.startup(function(use) --XXX package
    use { 'williamboman/mason-lspconfig.nvim', config = function()
       require 'mason-lspconfig'.setup { ensure_installed = { 'sumneko_lua', 'rust_analyzer@nightly', 'html', 'taplo' } }
       local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
       require 'mason-lspconfig'.setup_handlers {
          function(server_name) require 'lspconfig'[server_name].setup { capabilities = capabilities } end
       }
