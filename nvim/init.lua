0db5f36e6974ec6c62b81d4409b9a388e745d703
sugiura-hiromichi <sugiura130418@icloud.com>
2022-10-23 22:33:15 +0900

    create(cmp.lua & lsp.lua): distribute init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,9 +1,9 @@
-vim.cmd 'colo kalisi'
+vim.cmd 'colo tokyonight'
 if vim.fn.expand('%:p') == '' then vim.cmd [[e $MYVIMRC]] end
 
 vim.opt.pumblend = 20
 vim.opt.relativenumber = true
-vim.opt.signcolumn = 'no'
+vim.opt.number=true
 vim.opt.softtabstop = 3
 vim.opt.shiftwidth = 3
 vim.opt.expandtab = true
@@ -27,6 +27,10 @@ vim.keymap.set('i', '<c-u>', '<c-c>v^s')
 vim.keymap.set('i', '<a-d>', '<right><c-c>ves')
 vim.keymap.set('i', '<a-f>', '<c-right>')
 vim.keymap.set('i', '<a-b>', '<c-left>')
+vim.keymap.set({ 'n', 'v' }, '<a-h>', '<c-w>h')
+vim.keymap.set({ 'n', 'v' }, '<a-j>', '<c-w>j')
+vim.keymap.set({ 'n', 'v' }, '<a-k>', '<c-w>k')
+vim.keymap.set({ 'n', 'v' }, '<a-l>', '<c-w>l')
 vim.keymap.set('n', 't', require 'telescope.builtin'.builtin) -- Telescope
 vim.keymap.set('n', '<space>o', require 'telescope.builtin'.lsp_document_symbols)
 vim.keymap.set('n', '<space>d', require 'telescope.builtin'.diagnostics)
@@ -34,6 +38,8 @@ vim.keymap.set('n', '<space>j', require 'telescope.builtin'.lsp_references) --`j
 vim.keymap.set('n', '<space>b', require 'telescope.builtin'.buffers)
 vim.keymap.set('n', '<space>e', require 'telescope'.extensions.file_browser.file_browser)
 vim.keymap.set('n', '<space>f', require 'telescope'.extensions.frecency.frecency)
+vim.keymap.set('n', '<space>c', '<cmd>TodoTelescope<cr>')
+vim.keymap.set('n', '<space>n', '<cmd>Telescope notify<cr>')
 vim.keymap.set({ 'n', 'v' }, '<space>a', vim.lsp.buf.code_action)
 vim.keymap.set('n', '<space>r', vim.lsp.buf.rename)
 vim.keymap.set('n', '<space>h', vim.lsp.buf.hover)
@@ -43,7 +49,17 @@ vim.keymap.set('n', '<c-k>', vim.diagnostic.goto_prev)
 require 'packer'.startup(function(use) --XXX package
    use 'wbthomason/packer.nvim'
    use 'nvim-lua/plenary.nvim'
-   use 'sugiura-hiromichi/vim-kalisi'
+   use 'folke/tokyonight.nvim'
+   use { 'folke/todo-comments.nvim', config = function() require 'todo-comments'.setup {} end }
+   use { 'folke/noice.nvim', event = 'VimEnter', config = function() require 'noice'.setup() end }
+   use 'MunifTanjim/nui.nvim'
+   use 'rcarriga/nvim-notify'
+   use { 'nvim-treesitter/nvim-treesitter', config = function()
+      require 'nvim-treesitter.configs'.setup {
+         auto_install = true,
+         additional_vim_regex_highlighting = false
+      }
+   end }
    use 'nvim-tree/nvim-web-devicons'
    use { 'windwp/nvim-autopairs', config = function() require 'nvim-autopairs'.setup { map_c_h = true } end }
    use { 'nvim-telescope/telescope.nvim', tag = '0.1.0', config = function() -- telescope
@@ -54,50 +70,20 @@ require 'packer'.startup(function(use) --XXX package
    use 'nvim-telescope/telescope-frecency.nvim'
    use 'nvim-telescope/telescope-file-browser.nvim'
    use 'kkharji/sqlite.lua'
-   use { 'williamboman/mason.nvim', config = function() require 'mason'.setup() end } -- lsp
-   use { 'williamboman/mason-lspconfig.nvim', config = function()
-      require 'mason-lspconfig'.setup { ensure_installed = { 'sumneko_lua', 'rust_analyzer@nightly' } }
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
-      require 'mason-lspconfig'.setup_handlers {
-         function(server_name) require 'lspconfig'[server_name].setup { capabilities = capabilities } end
-      }
-   end }
+   use 'williamboman/mason.nvim'
+   use 'williamboman/mason-lspconfig.nvim'
    use 'neovim/nvim-lspconfig'
-   use { 'hrsh7th/nvim-cmp', config = function()
-      local luasnip = require 'luasnip'
-      local cmp = require 'cmp'
-      cmp.setup {
-         snippet = { expand = function(args) luasnip.lsp_expand(args.body) end, },
-         mapping = cmp.mapping.preset.insert({
-            ['<c-u>'] = cmp.mapping.scroll_docs(-10),
-            ['<c-d>'] = cmp.mapping.scroll_docs(10),
-            ['<c-c>'] = cmp.mapping.abort(),
-            ['<tab>'] = cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true, },
-            ['<c-n>'] = cmp.mapping(function(fallback)
-               if cmp.visible() then
-                  cmp.select_next_item()
-               elseif luasnip.expand_or_jumpable() then
-                  luasnip.expand_or_jump()
-               else
-                  fallback()
-               end
-            end, { 'i', 's' }),
-            ['<c-p>'] = cmp.mapping(function(fallback)
-               if cmp.visible() then
-                  cmp.select_prev_item()
-               elseif luasnip.jumpable(-1) then
-                  luasnip.jump(-1)
-               else
-                  fallback()
-               end
-            end, { 'i', 's' }),
-         }),
-         sources = { { name = 'nvim_lsp' }, { name = 'nvim_lua' }, { name = 'luasnip' }, },
-      }
-   end }
+   use 'hrsh7th/nvim-cmp'
    use 'hrsh7th/cmp-nvim-lsp'
    use 'hrsh7th/cmp-nvim-lua'
+   use 'hrsh7th/cmp-nvim-lsp-signature-help'
+   use 'hrsh7th/cmp-buffer'
+   use 'hrsh7th/cmp-path'
+   use 'hrsh7th/cmp-cmdline'
+   use 'hrsh7th/cmp-nvim-lsp-document-symbol'
    use 'saadparwaiz1/cmp_luasnip'
    use 'L3MON4D3/LuaSnip'
 end)
+
+require 'lsp'
+require 'cmp'
