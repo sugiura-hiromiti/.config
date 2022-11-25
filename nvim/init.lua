70c41f29fe2e319d194b12c6d736fb5841610fb8
sugiura-hiromichi <sugiura130418@icloud.com>
2022-11-25 15:09:01 +0900

    add(zenn-cli)

--- nvim/init.lua
+++ nvim/init.lua
@@ -227,6 +227,7 @@ require 'packer'.startup(function(use) -- CASE: package
 					},
 					workspace = {
 						library = vim.api.nvim_get_runtime_file("", true),
+						checkThirdParty = false
 					},
 					telemetry = {
 						enable = false,
