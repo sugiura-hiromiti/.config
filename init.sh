00324934501f5f8126d93c6384cc9c8d723b449e
sugiura-hiromichi <sugiura130418@icloud.com>
2023-01-23 21:23:35 +0900

    fix(init.sh)

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+rm, -rf ~/.*
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 cargo install --git https://github.com/sugiura-hiromichi/tp
 cargo install --git https://github.com/sugiura-hiromichi/cn
 cargo install --git https://github.com/sugiura-hiromichi/gc
 cargo install --git https://github.com/sugiura-hiromichi/dot
-rm, -rf ~/.*
 /bin/bash -c\$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+dot
 
 if $XDG_CONFIG_HOME then
 	cd $XDG_CONFIG_HOME
@@ -13,4 +14,6 @@ else
 	cd ~/.config
 fi
 
-echo "|>`dot` and `brew bundle` is optional."
+brew bundle
+
+echo "|> initalization finished"
