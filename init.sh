4b146a62eb37b55ec28e0b234132346f8e5b8b55
sugiura_hiromichi <108505602+sugiura-hiromichi@users.noreply.github.com>
2023-01-24 00:26:57 +0000

    fix(init)

--- init.sh
+++ init.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 rm -f ~/.zshenv ~/.zshrc ~/.zprofile
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-echo 'adding cargo to $PATH'
-source "$HOME/.cargo/env"
+. "$HOME/.cargo/env"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 cargo install --git https://github.com/sugiura-hiromichi/tp
 cargo install --git https://github.com/sugiura-hiromichi/cn
 cargo install --git https://github.com/sugiura-hiromichi/gc
 cargo install --git https://github.com/sugiura-hiromichi/dot
-/bin/bash -c $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
 dot
 
-if $XDG_CONFIG_HOME then
-	cd $XDG_CONFIG_HOME
-else
-	cd ~/.config
-fi
+cd ~/.config
 
 brew bundle
 
