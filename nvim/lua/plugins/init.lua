148f5130046d9e8e29e4a6cb7f3291b0a08f5207
sugiura-hiromichi <sugiura130418@icloud.com>
2023-11-11 17:09:52 +0900

    archive(heirline)

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -15,7 +15,6 @@ return {
 		'saimo/peek.nvim',
 		build = 'deno task --quiet build:fast',
 		opts = { theme = vim.o.background, app = 'browser' },
-		--config = function() require('peek').setup end,
 	},
 	'chrisgrieser/nvim-spider',
 	{
@@ -23,45 +22,41 @@ return {
 		cmd = 'Copilot',
 		event = 'InsertEnter',
 		opts = {
-			-- for `copilot-cmp`, `panel` and `suggestion` should be disabled
 			panel = { enable = false },
 			suggestion = {
 				enable = false,
-				--auto_trigger = true,
-				--keymap = { next = '<down>', prev = '<up>', dismiss = '<c-c>' },},
-				--filetypes = { ['*'] = true },
 			},
 		},
-		--config = function() require('copilot').setup , } end,
 	},
+	--	{
+	--		'catppuccin/nvim',
+	--		name = 'catppuccin',
+	--		opts = {
+	--			--	flavour = 'latte',
+	--			--			background = { light = 'latte', dark = 'frappe' },
+	--			transparent_background = true,
+	--			styles = {
+	--				comments = { 'italic' },
+	--				conditionals = { 'italic', 'bold' },
+	--				variables = { 'italic' },
+	--			},
+	--		},
+	--	},
 	{
-		'kawre/leetcode.nvim',
-		build = ':TSUpdate html',
-		dependencies = {
-			'nvim-treesitter/nvim-treesitter',
-			'nvim-telescope/telescope.nvim',
-			'nvim-lua/plenary.nvim',
-			'MunifTanjim/nui.nvim',
-			'rcarriga/nvim-notify',
-			'nvim-tree/nvim-web-devicons',
-		},
+		'rmehri01/onenord.nvim',
 		opts = {
-			lang = 'rust',
-			sql = 'sqlite3',
-		},
-	},
-	{
-		'catppuccin/nvim',
-		name = 'catppuccin',
-		opts = {
-			flavour = 'latte',
-			background = { light = 'latte', dark = 'frappe' },
 			styles = {
-				comments = { 'italic' },
-				conditionals = { 'italic', 'bold' },
-				variables = { 'italic' },
+				comments = 'italic',
+				strings = 'bold',
+				keywords = 'NONE',
+				diagnostics = 'underline',
+			},
+			disable = {
+				background = true, -- Disable setting the background color
+				float_background = true, -- Disable setting the background color for floating windows
+				cursorline = false, -- Disable the cursorline
+				eob_lines = true, -- Hide the end-of-buffer lines
 			},
 		},
-		--config = function() vim.cmd 'colo catppuccin' end,
 	},
 }
