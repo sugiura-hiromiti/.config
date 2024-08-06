f535006f9652375fe7a81d7032acbc32c56c2789
sugiura-hiromichi <sugiura130418@icloud.com>
2024-08-06 17:00:32 +0900

    implement heirline.lua

--- nvim/lua/plugins/heirline.lua
+++ nvim/lua/plugins/heirline.lua
@@ -2,21 +2,27 @@ return {
 	'rebelot/heirline.nvim',
 	config = function()
 		local symbols = require 'symbols'
-		local utils = require 'heirline.utils'
+		local hl = require('heirline.utils').get_highlight
 		local cond = require 'heirline.conditions'
-		local status_git = vim.b.gitsigns_status_dict
 
 		local align = { provider = '%=' }
-		local mode =
-			{ provider = vim.fn.mode() .. ' ', update = 'ModeChanged', hl = { bg = utils.get_highlight('Normal').fg } }
-		local path = {
-			pick_child = { 2, 1 },
-			fallthrough = false,
-			{ provider = '%Y' },
+		local mode = {
+			{ provider = vim.fn.mode() .. ' ', update = 'ModeChanged' },
 			{
 				provider = function()
-					local path = vim.api.nvim_buf_get_name(0)
-
+					return require('hydra.statusline').get_name() .. ' '
+				end,
+				condition = function()
+					return require('hydra.statusline').is_active()
+				end,
+			},
+		}
+		local path = {
+			provider = function()
+				local path = vim.api.nvim_buf_get_name(0)
+				if path == '' then
+					path = vim.bo.ft
+				else
 					local slash_pos = path:find '/'
 					local next_slash_pos = path:find('/', slash_pos + 1)
 					while next_slash_pos do
@@ -24,13 +30,10 @@ return {
 						slash_pos = path:find '/'
 						next_slash_pos = path:find('/', slash_pos + 1)
 					end
+				end
 
-					return path
-				end,
-				condition = function()
-					return vim.api.nvim_buf_get_name(0) ~= ''
-				end,
-			},
+				return path
+			end,
 		}
 		local navic = {
 			provider = function()
@@ -63,33 +66,81 @@ return {
 					-- 0 is just another output, we can decide to print it or not!
 					return self.errors > 0 and (self.error_icon .. self.errors .. ' ')
 				end,
-				hl = { fg = utils.get_highlight('DiagnosticError').fg },
+				hl = { fg = hl('DiagnosticError').fg },
 			},
 			{
 				provider = function(self)
 					return self.warnings > 0 and (self.warn_icon .. self.warnings .. ' ')
 				end,
-				hl = { fg = utils.get_highlight('DiagnosticWarn').fg },
+				hl = { fg = hl('DiagnosticWarn').fg },
 			},
 			{
 				provider = function(self)
 					return self.info > 0 and (self.info_icon .. self.info .. ' ')
 				end,
-				hl = { fg = utils.get_highlight('DiagnosticInfo').fg },
+				hl = { fg = hl('DiagnosticInfo').fg },
 			},
 			{
 				provider = function(self)
 					return self.hints > 0 and (self.hint_icon .. self.hints)
 				end,
-				hl = { fg = utils.get_highlight('DiagnosticHint').fg },
+				hl = { fg = hl('DiagnosticHint').fg },
 			},
 			{
 				provider = ']',
 			},
 		}
-		local git = { condition = cond.is_git_repo }
+		local git = {
+			condition = cond.is_git_repo,
+
+			init = function(self)
+				self.status_dict = vim.b.gitsigns_status_dict
+				self.has_changes = self.status_dict.added ~= 0
+					or self.status_dict.removed ~= 0
+					or self.status_dict.changed ~= 0
+			end,
+			{ -- git branch name
+				provider = function(self)
+					return ' ' .. self.status_dict.head
+				end,
+				hl = { bold = true },
+			},
+			{
+				condition = function(self)
+					return self.has_changes
+				end,
+				provider = '(',
+			},
+			{
+				provider = function(self)
+					local count = self.status_dict.added or 0
+					return count > 0 and ('+' .. count)
+				end,
+				hl = { fg = hl('GitSignsAdd').fg },
+			},
+			{
+				provider = function(self)
+					local count = self.status_dict.removed or 0
+					return count > 0 and ('-' .. count)
+				end,
+				hl = { fg = hl('GitSignsDelete').fg },
+			},
+			{
+				provider = function(self)
+					local count = self.status_dict.changed or 0
+					return count > 0 and ('~' .. count)
+				end,
+				hl = { fg = hl('GitSignsChange').fg },
+			},
+			{
+				condition = function(self)
+					return self.has_changes
+				end,
+				provider = ')',
+			},
+		}
 		local location = {
-			provider = '%l/%L:%c',
+			provider = ' %l:%c',
 		}
 
 		require('heirline').setup { statusline = { mode, path, navic, align, diag, git, location } }
