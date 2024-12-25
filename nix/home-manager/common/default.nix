1475e297b2f5d9c7efc90031fdd09407567426a9
sugiura-hiromichi <sugiura130418@icloud.com>
2024-12-25 21:08:00 +0900

    add nix dir

---
+++ nix/home-manager/common/default.nix
@@ -0,0 +1,102 @@
+{ pkgs, ... }@inputs:{
+	nixpkgs = {
+		config = {
+			allowUnfree = true;
+		};
+		overlays = [
+			inputs.inputs.rust-overlay.overlays.default
+		];
+	};
+	home = {
+		stateVersion = "24.05";
+	};
+	wayland = {
+		windowManager = {
+			hyprland = {
+				enable =true;
+			};
+		};
+	};
+	home = {
+		packages = with pkgs; [
+			ripgrep
+			eza
+			clang
+			starship
+			lua-language-server
+			marksman
+			deno
+			nodejs
+			rm-improved
+			skim
+			fd
+			gitui
+			stylua
+			wabt
+			zoxide
+			gh
+			rust-bin.nightly.latest.complete
+			neovim
+		];
+	};
+	programs = {
+		home-manager = {
+			enable = true;
+		};
+		git = {
+			enable = true;
+			userName = "sugiura-hiromiti";
+			userEmail = "sugiura130418@icloud.com";
+		};
+		zsh = {
+			enable = true;
+			autocd = true;
+			syntaxHighlighting = {
+				enable = true;
+			};
+			shellAliases = {
+				n = "nvim";
+				s = "eza";
+				zo = "z $OLDPWD";
+				wh = "which -a";
+			};
+		};
+		ripgrep = {
+			enable = true;
+		};
+		eza = {
+			enable = true;
+			git = true;
+			icons = true;
+			extraOptions = [
+				"-a"
+				"-h"
+				"-l"
+				"--group-directories-first"
+				"--sort=extension"
+				"--time-style=iso"
+				"--no-user"
+				"--no-time"
+			];
+		};
+		starship = {
+			enable = true;
+		};
+		skim = {
+			enable =true;
+			defaultCommand = "fd";
+			changeDirWidgetCommand = "fd --type d";
+			fileWidgtCommand = "fd --type f";
+		};
+		fd = {
+			enable = true;
+			hidden = true;
+		};
+		gitui = {
+			enable = true;
+		};
+		zoxide = {
+			enable = true;
+		};
+	};
+}
---
+++ nix/hosts/utm_nix_a/default.nix
@@ -0,0 +1,62 @@
+{ config, lib, pkgs, inputs, ... }:{
+	imports =[
+		./hardware-configuration.nix
+	];
+	users = {
+		users = {
+			utm_nix_a = {
+				isNormalUser = true;
+				description = "default user";
+				extraGroups = ["wheel" "networkmanager"];
+				shell = pkgs.zsh;
+			};
+		};
+	};
+	networking = {
+		networkmanager ={
+			enable = true;
+		};
+		hostName = "utm_nix_a";
+		wireless = {
+			enable =true;
+		};
+	};
+	boot = {
+		loader = {
+			systemd-boot = {
+				enable = true;
+			};
+			efi = {
+				canTouchEfiVariables = true;
+			};
+		};
+	};
+	time = {
+		timeZone = "Asia/Tokyo";
+	};
+	environment= {
+		systemPackages = with pkgs; [
+			wget
+			curl
+			zsh
+			git
+		];
+		variables={
+			EDITOR="nvim";
+		};
+	};
+	system = {
+		copySystemConfiguration = true;
+		stateVersion = "24.05";
+	};
+	programs = {
+		zsh = {
+			enable = true;
+		};
+	};
+	nix = {
+		settings = {
+			experimental-features = ["nix-command" "flakes"];
+		};
+	};
+}
