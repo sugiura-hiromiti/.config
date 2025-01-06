68fb968460768e7b8d3e6c0c6f5b309be976e5b0
sugiura-hiromiti <sugiura130418@icloud.com>
2025-01-06 20:54:01 +0900

    nix

--- nix/os/common/default.nix
+++ nix/os/common/default.nix
@@ -1,9 +1,18 @@
 {pkgs, ...}:{
+	nixpkgs = {
+		config = {
+			allowUnfree = true;
+			allowBroken = true;
+		};
+	};
 	environment={
 		systemPackages = with pkgs; [
 			curl
 			wget
 			git
+			discord-ptb
+			docker
+			slack
 		];
 	};
 }
