643ed071969ef7700cff27003cfc5ba851f0d3eb
sugiura-hiromiti <sugiura130418@icloud.com>
2025-01-08 00:29:09 +0900

    nix

--- nix/os/common/default.nix
+++ nix/os/common/default.nix
@@ -7,18 +7,19 @@
 	};
 	environment={
 		systemPackages = with pkgs; [
-			curl
-			wget
-			git
-			discord-ptb
-			docker
-			slack
-			dprint
-			lua
-			lua-language-server
-			marksman
-			taplo
-			google-java-format
+			# curl
+			# wget
+			# git
+			# discord-ptb
+			# docker
+			# slack
+			# dprint
+			# lua
+			# lua-language-server
+			# marksman
+			# taplo
+			# google-java-format
+			# zellij
 		];
 	};
 }
