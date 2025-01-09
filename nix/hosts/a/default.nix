9efd10845352f87bd99e59452bfa329d4ea0281d
sugiura-hiromiti <sugiura130418@icloud.com>
2025-01-09 09:38:04 +0900

    a

--- nix/hosts/a/default.nix
+++ nix/hosts/a/default.nix
@@ -2,16 +2,6 @@
 	imports=[
 		../../home-manager/darwin
 	];
-	users = {
-		users = {
-			a = {
-				#isNormalUser = true;
-				description = "default user";
-				#extraGroups = ["wheel" "networkmanager"];
-				shell = pkgs.zsh;
-			};
-		};
-	};
 	home = {
 		username = "a";
 		homeDirectory = "/Users/a";
