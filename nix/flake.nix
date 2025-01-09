663919ab9b4bf59f02df7d1a4ae216c342ca2e86
sugiura-hiromiti <sugiura130418@icloud.com>
2025-01-09 09:47:15 +0900

    fix

--- nix/flake.nix
+++ nix/flake.nix
@@ -31,37 +31,37 @@
 		};
 	};
 	outputs={nixpkgs,home-manager, nix-darwin, pkgs, ...} @inputs :
-	let
-		# make sure hostname is equal to username
-		mkPlatform = { cpu, os, name, hmModule, setter }: setter {
-			system = cpu + "-" + os;
-			users = {
-				users = {
-					${name} = {
-						#isNormalUser = true;
-						description = "default user";
-						#extraGroups = ["wheel" "networkmanager"];
-						shell = pkgs.zsh;
-					};
-				};
-			};
-			modules = [
-				(./os + "/${os}")
-				hmModule.home-manager {
-					home-manager = {
-						extraSpecialArgs = {
-							inherit inputs;
-						};
-					};
-					home-manager = {
-						users = {
-							${name} = import (./hosts + "/${name}");
-						};
-					};
-				}
-			];
-		};
-	in
+	# let
+	# 	# make sure hostname is equal to username
+	# 	mkPlatform = { cpu, os, name, hmModule, setter }: setter {
+	# 		system = cpu + "-" + os;
+	# 		users = {
+	# 			users = {
+	# 				${name} = {
+	# 					#isNormalUser = true;
+	# 					description = "default user";
+	# 					#extraGroups = ["wheel" "networkmanager"];
+	# 					shell = pkgs.zsh;
+	# 				};
+	# 			};
+	# 		};
+	# 		modules = [
+	# 			(./os + "/${os}")
+	# 			hmModule.home-manager {
+	# 				home-manager = {
+	# 					extraSpecialArgs = {
+	# 						inherit inputs;
+	# 					};
+	# 				};
+	# 				home-manager = {
+	# 					users = {
+	# 						${name} = import (./hosts + "/${name}");
+	# 					};
+	# 				};
+	# 			}
+	# 		];
+	# 	};
+	# in
 	{
 		nixpkgs = {
 			config = {
@@ -69,46 +69,47 @@
 			};
 		};
 		nixosConfigurations={
-			# utm_nix_a=nixpkgs.lib.nixosSystem{
-			# 	system="aarch64-linux";
-			# 	modules=[
-			# 		./hosts/utm_nix_a
-			# 		home-manager.nixosModules.home-manager{
-			# 			home-manager.extraSpecialArgs = {
-			# 				inherit inputs;
-			# 			};
-			# 			home-manager.users.utm_nix_a = import ./home-manager/linux;
-			# 		}
-			# 	];
-			# };
-				utm_nix_a = mkPlatform {
-					cpu="aarch64";
-					os= "linux";
-					name="utm_nix_a";
-					hmModule	= home-manager.nixosModules;
-					setter=nixpkgs.lib.nixosSystem;
-				};
+			utm_nix_a=nixpkgs.lib.nixosSystem{
+				system="aarch64-linux";
+				modules=[
+					./hosts/utm_nix_a
+					home-manager.nixosModules.home-manager{
+						home-manager.extraSpecialArgs = {
+							inherit inputs;
+						};
+						home-manager.users.utm_nix_a = import ./home-manager/linux;
+					}
+				];
+			};
+				# utm_nix_a = mkPlatform {
+				# 	cpu="aarch64";
+				# 	os= "linux";
+				# 	name="utm_nix_a";
+				# 	hmModule	= home-manager.nixosModules;
+				# 	setter=nixpkgs.lib.nixosSystem;
+				# };
 		};
 		darwinConfigurations = {
-			# a = nix-darwin.lib.darwinSystem {
-			# 	system = "aarch64-darwin";
-			# 	modules = [
-			# 		./hosts/a
-			# 		home-manager.darwinModules.home-manager{
-			# 			home-manager.extraSpecialArgs = {
-			# 				inherit inputs;
-			# 			};
-			# 			home-manager.users.a = import ./home-manager/darwin;
-			# 		}
-			# 	];
-			# };
-			a = mkPlatform {
-				cpu="aarch64";
-				os= "darwin";
-				name="a";
-				hmModule = home-manager.darwinModules;
-				setter=nix-darwin.lib.darwinSystem;
+			a = nix-darwin.lib.darwinSystem {
+				system = "aarch64-darwin";
+				modules = [
+					./hosts/a
+					# home-manager.darwinModules.home-manager{
+					# 	home-manager.extraSpecialArgs = {
+					# 		inherit inputs;
+					# 	};
+					# 	home-manager.users.a = import ./home-manager/darwin;
+					# }
+				];
+				specialArgs={inherit	inputs;};
 			};
+			# a = mkPlatform {
+			# 	cpu="aarch64";
+			# 	os= "darwin";
+			# 	name="a";
+			# 	hmModule = home-manager.darwinModules;
+			# 	setter=nix-darwin.lib.darwinSystem;
+			# };
 		};
 	};
 }
