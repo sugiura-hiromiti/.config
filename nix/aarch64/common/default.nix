837940a01d050a31b3270a4f4c0ac6d9bcee416c
sugiura-hirmiti <sugiura130418@icloud.com>
2025-05-25 15:52:52 +0900

    sync

--- nix/aarch64/common/default.nix
+++ nix/aarch64/common/default.nix
@@ -2,18 +2,16 @@
 {pkgs, neovim-nightly-overlay, home-manager, ...}:{
 	imports=[
 		../../common/common
-		home-manager.darwinModules.home-manager
 	];
 
-	home-manager ={
-				useGlobalPkgs=true;
-				useUserPackages=true;
-				users = {
-					a = {
-						home = {
-							packages = with pkgs; [
-
-							];
+	home-manager.darwinModules.home-manager ={
+		useGlobalPkgs=true;
+			useUserPackages=true;
+			users = {
+				a = {
+					home = {
+						packages = with pkgs; [
+					];
 				};
 			};
 		};
