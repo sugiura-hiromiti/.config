7281b961aebdc3b3ba46c685708d737988bdab88
sugiura-hirmiti <sugiura130418@icloud.com>
2025-05-29 19:33:59 +0900

    sync

--- sketchybar/item/alias.lua
+++ sketchybar/item/alias.lua
@@ -1,9 +1,4 @@
-local raycast_calendar_event =
-	SBAR.add('alias', 'Raycast,raycastCalendarStatusItem', { position = 'right', y_offset = 2 })
--- local obsidian = SBAR.add(
--- 	'alias',
--- 	'Raycast,extension_obsidian_obsidianMenuBar__6090553e-ee96-4c89-af5c-e6c60fa551ba',
--- 	{ position = 'right' }
--- )
-local clock = SBAR.add('alias', 'Control Center,Clock', { position = 'right' })
-local text_input_menu = SBAR.add('alias', 'TextInputMenuAgent,Item-0', { position = 'right' })
+local text_input_menu = SBAR.add('alias', 'TextInputMenuAgent,Item-0', { position = 'left' })
+local raycast_calendar_event = SBAR.add('alias', 'Raycast,raycastCalendarStatusItem', { y_offset = 2 })
+-- local obsidian =
+-- 	SBAR.add('alias', 'Raycast,extension_obsidian_obsidianMenuBar__6090553e-ee96-4c89-af5c-e6c60fa551ba', {})
