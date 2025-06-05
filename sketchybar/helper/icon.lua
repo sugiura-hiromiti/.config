714eb45cba8523b505db61783f5866c6d8cb121a
sugiura-hiromiti <sugiura130418@icloud.com>
2025-06-05 16:45:22 +0900

    sync

--- sketchybar/helper/icon.lua
+++ sketchybar/helper/icon.lua
@@ -1,16 +1,20 @@
-return {
+local icons = {
 	battery = {
-		_10 = '\u{db80}\u{dc7a}',
-		_20 = '',
-		_30 = '',
-		_40 = '',
-		_50 = '',
-		_60 = '',
-		_70 = '',
-		_80 = '',
-		_90 = '',
-		_100 = '',
-		charging='',
+		_10 = '\u{c007a}',
+		_20 = '\u{c007b}',
+		_30 = '\u{c007c}',
+		_40 = '\u{c007d}',
+		_50 = '\u{c007e}',
+		_60 = '\u{F3}\u{B0}\u{81}\u{BF}',
+		_70 = '\u{F3}\u{B0}\u{82}\u{80}',
+		_80 = '\u{F3}\u{B0}\u{82}\u{81}',
+		_90 = '\u{F3}\u{B0}\u{82}\u{82}',
+		_100 = '\u{F3}\u{B0}\u{81}\u{B9}',
+		charging = '\u{F3}\u{B0}\u{82}\u{84}',
 	},
-	error = '',
+	error = '\u{EE}\u{AA}\u{87}',
 }
+
+require('helper.debug_util').print_table(icons)
+
+return icons
