d48888abbf8fa4c64c2259193c400f6f88ac3292
sugiura-hiromiti <sugiura130418@icloud.com>
2025-06-19 20:57:54 +0900

    fix args

--- nix/darwin/aarch64/default.nix
+++ nix/darwin/aarch64/default.nix
@@ -1,9 +1,9 @@
 # darwin/aarch64
-{legacy}:
-let
-	common = (import ../common {inherit legacy;});
-	arch = (import ../../common/aarch64 {inherit legacy;});
+{legacy, ...} @ inputs: let
+  common = import ../common {inherit legacy;};
+  arch = import ../../common/aarch64 {inherit legacy;};
 in
-	 common  ++ arch  ++
-	[
-]
+  common
+  ++ arch
+  ++ [
+  ]
