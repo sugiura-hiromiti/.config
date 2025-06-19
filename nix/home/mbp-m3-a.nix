5ae067b266bc854889d27617653c1cb299194cb8
sugiura-hiromiti <sugiura130418@icloud.com>
2025-06-19 21:00:14 +0900

    fix args

---
+++ nix/home/mbp-m3-a.nix
@@ -0,0 +1,29 @@
+{
+  inputs,
+  lib,
+  config,
+  pkgs,
+}:
+let
+  username = "a";
+  mypkgs = import ../darwin/common { inherit pkgs; };
+in
+{
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  home = {
+    username = username;
+    homeDirectory = "/Users/${username}";
+    stateVersion = "unstable";
+    packages = mypkgs ++ [ ];
+  };
+  programs = {
+    home-manager = {
+      enable = true;
+    };
+  };
+}
