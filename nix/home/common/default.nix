147320bb570cb06291fd7bab67f7e6e2b47b4e62
sugiura-hiromiti <sugiura130418@icloud.com>
2025-06-20 09:57:56 +0900

    fix(nix/home-manager): extract common user configs to home/common/default.nix

---
+++ nix/home/common/default.nix
@@ -0,0 +1,55 @@
+{
+  username,
+  mypkgs,
+  ...
+}: {
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+    };
+  };
+  home = {
+    username = username;
+    homeDirectory = "/Users/${username}";
+    stateVersion = "25.11";
+    file = {
+      ".clang-format" = {
+        target = ".clang-format";
+        source = ../../.clang-format;
+      };
+      ".editorconfig" = {
+        target = ".editorconfig";
+        source = ../../.editorconfig;
+      };
+      ".gitconfig" = {
+        target = ".gitconfig";
+        source = ../../.gitconfig;
+      };
+      ".gitconfig_p" = {
+        target = ".gitconfig_p";
+        source = ../../.gitconfig_p;
+      };
+      ".dprint.json" = {
+        target = ".dprint.json";
+        source = ../../.dprint.json;
+      };
+      ".rustfmt.toml" = {
+        target = ".rustfmt.toml";
+        source = ../../.rustfmt.toml;
+      };
+      ".stylua.toml" = {
+        target = ".stylua.toml";
+        source = ../../.stylua.toml;
+      };
+      ".zshenv" = {
+        target = ".zshenv";
+        source = ../../.zshenv;
+      };
+      ".zshrc" = {
+        target = ".zshrc";
+        source = ../../.zshrc;
+      };
+    };
+    packages = mypkgs;
+  };
+}
