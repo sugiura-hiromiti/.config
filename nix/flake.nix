02c36ef302e358924fd3a32793b278f89b9bc7f1
sugiura-hiromiti <sugiura130418@icloud.com>
2025-07-11 17:52:15 +0900

    feat(.zshenv): improve shell history and env var handling

--- nix/flake.nix
+++ nix/flake.nix
@@ -25,96 +25,98 @@
       url = "github:nix-community/neovim-nightly-overlay";
     };
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      neovim-nightly-overlay,
-      home-manager,
-      nix-darwin,
-    }@inputs:
-    let
-      secret = import ./secret.nix { };
-      user = builtins.replaceStrings [ "." ] [ "-" ] secret.username;
-      system = secret.system;
-      user-system = "${user}-${system}";
-      nixpkgs-overlayed = import nixpkgs {
-        overlays = [ inputs.neovim-nightly-overlay.overlays.default ];
-        inherit system;
-      };
-    in
-    {
-      homeConfigurations = {
-        conf = home-manager.lib.homeManagerConfiguration {
-          pkgs = nixpkgs-overlayed;
-          extraSpecialArgs = {
-            inherit inputs;
-          };
-          modules = [
-            ./home/${user-system}.nix
-            # ./darwin/aarch64
-          ];
+  outputs = {
+    self,
+    nixpkgs,
+    neovim-nightly-overlay,
+    home-manager,
+    nix-darwin,
+  } @ inputs: let
+    secret = import ./secret.nix {};
+    user = builtins.replaceStrings ["."] ["-"] secret.username;
+    system = secret.system;
+    user-system = "${user}-${system}";
+    nixpkgs-overlayed = import nixpkgs {
+      overlays = [inputs.neovim-nightly-overlay.overlays.default];
+      inherit system;
+    };
+  in {
+    homeConfigurations = {
+      conf = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs-overlayed;
+        extraSpecialArgs = {
+          inherit inputs;
         };
-      };
-
-      darwinConfigurations = {
-        conf = nix-darwin.lib.darwinSystem {
-          inherit system;
-          modules = [
-            ./nix-darwin/${user-system}.nix
-          ];
-        };
-      };
-
-      apps.${system}.update = {
-        type = "app";
-        program = toString (
-          nixpkgs-overlayed.writeShellScript "update-script" ''
-            set -e
-            echo "
-            -------------------------------------------"
-            echo "user: ${user}"
-            echo "system: ${system}"
-            echo "-------------------------------------------
-            "
-            git stage .
-            echo "
-
-            -------------------------------------------"
-            echo "updating flake"
-            echo "-------------------------------------------
-            "
-            nix flake update
-            echo "
-
-            -------------------------------------------"
-            echo "updating home-manager"
-            echo "-------------------------------------------
-            "
-            nix run nixpkgs#home-manager -- switch --flake .#conf
-
-            echo "
-
-            -------------------------------------------"
-            echo "updating nix-darwin"
-            echo "-------------------------------------------
-            "
-            sudo nix run nix-darwin -- switch --flake .#conf
-            echo "
-
-            -------------------------------------------"
-            echo "cleaning up store"
-            echo "-------------------------------------------
-            "
-
-            echo "
-
-            -------------------------------------------"
-            echo "update complete!"
-            echo "-------------------------------------------
-            "
-          ''
-        );
+        modules = [
+          ./home/${user-system}.nix
+          # ./darwin/aarch64
+        ];
       };
     };
+
+    darwinConfigurations = {
+      conf = nix-darwin.lib.darwinSystem {
+        inherit system;
+        modules = [
+          ./nix-darwin/${user-system}.nix
+        ];
+      };
+    };
+
+    apps.${system}.update = {
+      type = "app";
+      program = toString (
+        nixpkgs-overlayed.writeShellScript "update-script" ''
+          set -e
+          echo "
+          -------------------------------------------"
+          echo "user: ${user}"
+          echo "system: ${system}"
+          echo "-------------------------------------------
+          "
+          git stage .
+          echo "
+
+          -------------------------------------------"
+          echo "updating flake"
+          echo "-------------------------------------------
+          "
+          nix flake update
+          echo "
+
+          -------------------------------------------"
+          echo "updating home-manager"
+          echo "-------------------------------------------
+          "
+          nix run nixpkgs#home-manager -- switch --flake .#conf
+
+          echo "
+
+          -------------------------------------------"
+          echo "updating nix-darwin"
+          echo "-------------------------------------------
+          "
+          sudo nix run nix-darwin -- switch --flake .#conf
+
+          echo "
+
+          -------------------------------------------"
+          echo "restarting launchd services"
+          echo "-------------------------------------------
+          "
+          yabai --stop-service
+          yabai --uninstall-service
+          yabai --install-service
+          yabai --start-service
+
+          echo "
+
+          -------------------------------------------"
+          echo "update complete!"
+          echo "-------------------------------------------
+          "
+        ''
+      );
+    };
+  };
 }
