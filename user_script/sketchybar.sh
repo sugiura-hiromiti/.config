11ed2a7d861c42c81ada42fe1ea7e341cb6e9df9
sugiura-hiromiti <sugiura130418@icloud.com>
2025-07-11 17:53:03 +0900

    feat(sketchybar): add script to start sketchybar

---
+++ user_script/sketchybar.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title sketchybar
+# @raycast.mode inline
+# @raycast.refreshTime 1d
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName sketchybar_daemon
+
+# Documentation:
+# @raycast.description sketchybar daemon
+# @raycast.author sugiura-hiromichi
+# @raycast.authorURL https://raycast.com/sugiura-hiromichi
+
+sketchybar
