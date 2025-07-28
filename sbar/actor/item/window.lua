aa8ef92f357679b6bad42d8f739e052b318afda3
sugiura-hiromiti <sugiura130418@icloud.com>
2025-07-28 12:11:08 +0900

    sync

--- sbar/actor/item/window.lua
+++ sbar/actor/item/window.lua
@@ -3,8 +3,8 @@ local palette = require 'actor.helper.color'
 local window_title = BAR.add('item', 'window_title', {
 	width = 'dynamic',
 	position = 'left',
-	background = { border_color = palette.lavender },
-	label = { color = palette.lavender },
+	background = { border_color = palette.green },
+	label = { color = palette.green },
 })
 
 BAR.add('event', 'window_title_changed')
