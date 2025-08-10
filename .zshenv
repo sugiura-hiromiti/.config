51031c0592540ead2b3430f383d2351fd212086a
sugiura-hiromiti <sugiura130418@icloud.com>
2025-08-10 23:57:48 +0900

    sync

--- .zshenv
+++ .zshenv
@@ -1,51 +1,32 @@
 # Amazon Q pre block. Keep at the top of this file.
 [[ -f "${HOME}/Library/Application Support/amazon-q/shell/zshrc.pre.zsh" ]] && builtin source "${HOME}/Library/Application Support/amazon-q/shell/zshrc.pre.zsh"
 
+
 set -o emacs
 setopt incappendhistory
-# autoload -Uz colors
-
 
 # setopt AUTO_CD
 # cdpath=(.. ~ ~/Downloads)
 
 . $HOME/.profile
 # source /opt/homebrew/opt/zsh-fast-syntax-highlighting/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
-[ -f "/Users/a/.ghcup/env" ] && . "/Users/a/.ghcup/env" # ghcup-env
 
 #environment variables
 export EDITOR=nvim
 export VISUAL=nvim
-export MANPAGER=less
+export MANPAGER=nvim
 export RIPGREP_CONFIG_PATH=$HOME/.config/rg/config
-export YABAI_CERT=yabai-cert
 export JAVA_HOME=/Library/Java/JavaVirtualMachines/openjdk.jdk/Contents/Home/
 export MY_CUSTOM_ENV_VARS_CURRENTLY_EXECUTING_PROMPT=''
-export COMMIT_SUMMARY_PATH=$HOME/.local/state/commit_summary
 
 #aliases
-# to remove alias in zsh, simply just remove it
 alias s='eza -ahlF --icons --group-directories-first --sort=extension --time-style=iso --git'
 alias n='nvim'
-#alias u='yabai --stop-service ; yabai --uninstall-service ; brew upgrade ;
-#echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai ;
-#yabai --start-service ;
-#rustup self update ; rustup update ; cargo install-update -a'
 alias zo='z $OLDPWD'
-# alias gi='git init ; git add . ; git commit -m'
 alias gl='git pull'
 alias gp='git push'
 alias gm='git merge'
-#alias bu='brew uninstall'
-#alias bi='brew install'
-#alias bl='brew list'
-#alias bs='brew search'
-#alias ba='brew abv'
-#alias bb='brew bundle dump --all -f'
 alias wh='which -a'
-#alias so='source'
-#alias dflt_a='defaults read > /tmp/dotfiles_init/a.log'
-#alias dflt_b='defaults read > /tmp/dotfiles_init/b.log ; diff /tmp/dotfiles_init/a.log /tmp/dotfiles_init/b.log'
 # alias zenn='cd ~/Downloads/zenn/articles/ ; my_target_file=$(sk -ic '"'rg "'"{}"'"'"'| sed s"/:published: false//") ; n $my_target_file'
 alias gac='~/.config/user_script/auto_commit.sh'
 alias u='(cd $HOME/.config/nix && \
@@ -78,10 +59,8 @@ function title() {
 }
 
 function chpwd_print_dir() {
-	if [[ $(pwd) != $HOME ]]; then
-		# alias `s` will be expanded
-		s
-	fi
+	# alias `s` will be expanded
+	s
 }
 
 function set_current_program_var() {
@@ -106,15 +85,10 @@ add-zsh-hook preexec set_current_program_var
 add-zsh-hook precmd clear_current_program_var
 
 export PATH=/opt/homebrew/bin:$PATH
-export PATH=$HOME/.cargo/bin:$PATH
 export PATH=$PATH:/Users/a/Library/Python/3.9/bin
-# eval "$(zoxide init zsh)"
-# eval "$(starship init zsh)"
-# export ZELLIJ_AUTO_ATTACH=true
-# eval "$(zellij setup --generate-auto-start zsh)"
 eval "$(direnv hook zsh)"
 
-# test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 
 # Amazon Q post block. Keep at the bottom of this file.
--- nix/pkg/default.nix
+++ nix/pkg/default.nix
@@ -5,92 +5,99 @@
   system,
   pkgs,
   fenix,
-}:
-(
+}: (
   with pkgs;
-  [
-    fenix.packages.${system}.latest.toolchain
-    git
-    curl
-    chibi
-    deno
-    dosfstools
-    dprint
-    eza
-    fd
-    gh
-    gitui
-    go
-    #jdk
-    jdk11
-    google-java-format
-    gopls
-    graphviz
-    jdt-language-server
-    lua5_4_compat
-    lua-language-server
-    luajitPackages.luarocks
-    marksman
-    markdown-oxide
-    maven
-    mysql84
-    nasm
-    neovim
-    ninja
-    nodejs-slim
-    pkgconf
-    ripgrep
-    rm-improved
-    sbcl
-    skim
-    starship
-    stylua
-    taplo
-    wabt
-    wasmtime
-    wget
-    zig
-    zoxide
-    zsh-fast-syntax-highlighting
-    nerd-fonts.meslo-lg
-    alejandra
-    asm-lsp
-    docker-language-server
-    docker-compose-language-service
-    haskell-language-server
-    vscode-langservers-extracted
-    typescript-language-server
-    zls
-    tailwindcss-language-server
-    phpactor
-    obsidian
-    neovide
-    reaper
-    zsh
-    nil
-    tailscale
-    sqls
-    bat
-    tokei
-    cargo-expand
-    dockerfile-language-server-nodejs
-    yaml-language-server
-    # sbarlua build dependencies
-    # clang
-    # libllvm
-    vue-language-server
-    sd
-    codex
-    act
-    direnv
-    amazon-q-cli
-    # sqlite
-  ]
-  ++ (if arch == "aarch64" then [ ] else [ ])
-  ++ (if arch == "x86_64" then [ ] else [ ])
-  ++ (
-    if os == "darwin" then
-      [
+    [
+      fenix.packages.${system}.latest.toolchain
+      git
+      curl
+      chibi
+      deno
+      dosfstools
+      dprint
+      eza
+      fd
+      gh
+      gitui
+      go
+      #jdk
+      jdk11
+      google-java-format
+      gopls
+      graphviz
+      jdt-language-server
+      lua5_4_compat
+      lua-language-server
+      luajitPackages.luarocks
+      marksman
+      markdown-oxide
+      maven
+      mysql84
+      nasm
+      neovim
+      ninja
+      nodejs-slim
+      pkgconf
+      ripgrep
+      rm-improved
+      sbcl
+      skim
+      starship
+      stylua
+      taplo
+      wabt
+      wasmtime
+      wget
+      zig
+      zoxide
+      zsh-fast-syntax-highlighting
+      nerd-fonts.meslo-lg
+      alejandra
+      asm-lsp
+      docker-language-server
+      docker-compose-language-service
+      haskell-language-server
+      vscode-langservers-extracted
+      typescript-language-server
+      zls
+      tailwindcss-language-server
+      phpactor
+      obsidian
+      neovide
+      reaper
+      zsh
+      nil
+      tailscale
+      sqls
+      bat
+      tokei
+      cargo-expand
+      yaml-language-server
+      # sbarlua build dependencies
+      # clang
+      # libllvm
+      vue-language-server
+      sd
+      codex
+      act
+      direnv
+      amazon-q-cli
+      zellij
+      # sqlite
+    ]
+    ++ (
+      if arch == "aarch64"
+      then []
+      else []
+    )
+    ++ (
+      if arch == "x86_64"
+      then []
+      else []
+    )
+    ++ (
+      if os == "darwin"
+      then [
         mas
         pngpaste
         jankyborders
@@ -104,33 +111,54 @@
         sbarlua
         ghostty-bin
       ]
-    else
-      [ ]
-  )
-  ++ (
-    if os == "linux" then
-      [
+      else []
+    )
+    ++ (
+      if os == "linux"
+      then [
         firefox
         docker
         ghostty
       ]
-    else
-      [ ]
-  )
-  ++ (if arch == "aarch64" && os == "darwin" then [ ] else [ ])
-  ++ (if arch == "x86_64" && os == "darwin" then [ ] else [ ])
-  ++ (if arch == "aarch64" && os == "linux" then [ slacky ] else [ ])
-  ++ (
-    if arch == "x86_64" && os == "linux" then
-      [
+      else []
+    )
+    ++ (
+      if arch == "aarch64" && os == "darwin"
+      then []
+      else []
+    )
+    ++ (
+      if arch == "x86_64" && os == "darwin"
+      then []
+      else []
+    )
+    ++ (
+      if arch == "aarch64" && os == "linux"
+      then [slacky]
+      else []
+    )
+    ++ (
+      if arch == "x86_64" && os == "linux"
+      then [
         discord
         slack
         vital
       ]
-    else
-      [ ]
-  )
-  ++ (if user == "a" then [ ] else [ ])
-  ++ (if user == "hiromichi-sugiura" then [ ] else [ ])
-  ++ (if user == "xsugiurah" then [ ] else [ ])
+      else []
+    )
+    ++ (
+      if user == "a"
+      then []
+      else []
+    )
+    ++ (
+      if user == "hiromichi-sugiura"
+      then []
+      else []
+    )
+    ++ (
+      if user == "xsugiurah"
+      then []
+      else []
+    )
 )
