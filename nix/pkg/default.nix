64ca4613cc8628ff1f5fb16dac05075766188006
sugiura-hiromiti <sugiura130418@icloud.com>
2025-08-20 11:23:30 +0900

    nix(pkg) add swift

--- nix/pkg/default.nix
+++ nix/pkg/default.nix
@@ -5,96 +5,89 @@
   system,
   pkgs,
   fenix,
-}: (
+}:
+(
   with pkgs;
-    [
-      fenix.packages.${system}.latest.toolchain
-      git
-      curl
-      chibi
-      deno
-      dosfstools
-      dprint
-      eza
-      fd
-      gh
-      gitui
-      go
-      #jdk
-      jdk11
-      google-java-format
-      gopls
-      graphviz
-      jdt-language-server
-      lua5_4_compat
-      lua-language-server
-      luajitPackages.luarocks
-      marksman
-      markdown-oxide
-      maven
-      mysql84
-      nasm
-      neovim
-      ninja
-      nodejs-slim
-      pkgconf
-      ripgrep
-      rm-improved
-      sbcl
-      skim
-      starship
-      stylua
-      taplo
-      wabt
-      wasmtime
-      wget
-      zig
-      zoxide
-      nerd-fonts.meslo-lg
-      alejandra
-      asm-lsp
-      docker-language-server
-      docker-compose-language-service
-      haskell-language-server
-      vscode-langservers-extracted
-      typescript-language-server
-      zls
-      tailwindcss-language-server
-      phpactor
-      obsidian
-      neovide
-      zsh
-      nil
-      tailscale
-      sqls
-      bat
-      tokei
-      cargo-expand
-      yaml-language-server
-      # sbarlua build dependencies
-      # clang
-      # libllvm
-      vue-language-server
-      sd
-      codex
-      direnv
-      amazon-q-cli
-      zellij
-      # sqlite
-    ]
-    ++ (
-      if arch == "aarch64"
-      then []
-      else []
-    )
-    ++ (
-      if arch == "x86_64"
-      then []
-      else []
-    )
-    ++ (
-      if os == "darwin"
-      then [
+  [
+    fenix.packages.${system}.latest.toolchain
+    git
+    curl
+    chibi
+    deno
+    dosfstools
+    dprint
+    eza
+    fd
+    gh
+    gitui
+    go
+    #jdk
+    jdk11
+    google-java-format
+    gopls
+    graphviz
+    jdt-language-server
+    lua5_4_compat
+    lua-language-server
+    luajitPackages.luarocks
+    marksman
+    markdown-oxide
+    maven
+    mysql84
+    nasm
+    neovim
+    ninja
+    nodejs-slim
+    pkgconf
+    ripgrep
+    rm-improved
+    sbcl
+    skim
+    starship
+    stylua
+    taplo
+    wabt
+    wasmtime
+    wget
+    zig
+    zoxide
+    nerd-fonts.meslo-lg
+    alejandra
+    asm-lsp
+    docker-language-server
+    docker-compose-language-service
+    haskell-language-server
+    vscode-langservers-extracted
+    typescript-language-server
+    zls
+    tailwindcss-language-server
+    phpactor
+    obsidian
+    neovide
+    zsh
+    nil
+    tailscale
+    sqls
+    bat
+    tokei
+    cargo-expand
+    yaml-language-server
+    # sbarlua build dependencies
+    # clang
+    # libllvm
+    vue-language-server
+    sd
+    codex
+    direnv
+    amazon-q-cli
+    zellij
+    swift
+  ]
+  ++ (if arch == "aarch64" then [ ] else [ ])
+  ++ (if arch == "x86_64" then [ ] else [ ])
+  ++ (
+    if os == "darwin" then
+      [
         mas
         pngpaste
         jankyborders
@@ -110,54 +103,33 @@
         skhd
         yabai
       ]
-      else []
-    )
-    ++ (
-      if os == "linux"
-      then [
+    else
+      [ ]
+  )
+  ++ (
+    if os == "linux" then
+      [
         firefox
         docker
         ghostty
       ]
-      else []
-    )
-    ++ (
-      if arch == "aarch64" && os == "darwin"
-      then []
-      else []
-    )
-    ++ (
-      if arch == "x86_64" && os == "darwin"
-      then []
-      else []
-    )
-    ++ (
-      if arch == "aarch64" && os == "linux"
-      then [slacky]
-      else []
-    )
-    ++ (
-      if arch == "x86_64" && os == "linux"
-      then [
+    else
+      [ ]
+  )
+  ++ (if arch == "aarch64" && os == "darwin" then [ ] else [ ])
+  ++ (if arch == "x86_64" && os == "darwin" then [ ] else [ ])
+  ++ (if arch == "aarch64" && os == "linux" then [ slacky ] else [ ])
+  ++ (
+    if arch == "x86_64" && os == "linux" then
+      [
         discord
         slack
         vital
       ]
-      else []
-    )
-    ++ (
-      if user == "a"
-      then []
-      else []
-    )
-    ++ (
-      if user == "hiromichi-sugiura"
-      then []
-      else []
-    )
-    ++ (
-      if user == "xsugiurah"
-      then []
-      else []
-    )
+    else
+      [ ]
+  )
+  ++ (if user == "a" then [ ] else [ ])
+  ++ (if user == "hiromichi-sugiura" then [ ] else [ ])
+  ++ (if user == "xsugiurah" then [ ] else [ ])
 )
