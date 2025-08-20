9ed15fd945784dee1d957a2a725c3b2281686bac
sugiura-hiromiti <sugiura130418@icloud.com>
2025-08-20 18:06:07 +0900

    sync

--- nvim/lua/plugins/util.lua
+++ nvim/lua/plugins/util.lua
@@ -36,6 +36,15 @@ return {
 			vim.cmd 'colo catppuccin'
 		end,
 	},
+	-- Lua
+	{
+		'f-person/auto-dark-mode.nvim',
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		},
+	},
 	-- NOTE: LSP
 	{
 		'onsails/lspkind.nvim',
