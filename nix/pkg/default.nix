1c3f7dfa63785337daaad69c72d48915e22fae45
sugiura-hiromiti <sugiura130418@icloud.com>
2025-09-01 19:40:24 -0700

    sync

--- nix/pkg/default.nix
+++ nix/pkg/default.nix
@@ -5,63 +5,56 @@
   system,
   pkgs,
   fenix,
-}: (
+}:
+(
   with pkgs;
-    [
-      fenix.packages.${system}.latest.toolchain
-      git
-      curl
-      dprint
-      eza
-      fd
-      gh
-      gitui
-      lua5_4_compat
-      lua-language-server
-      luajitPackages.luarocks
-      marksman
-      markdown-oxide
-      neovim
-      ripgrep
-      rm-improved
-      skim
-      starship
-      stylua
-      taplo
-      zoxide
-      nerd-fonts.meslo-lg
-      # alejandra
-      obsidian
-      zsh
-      tailscale
-      bat
-      tokei
-      cargo-expand
-      yaml-language-server
-      # sbarlua build dependencies
-      # clang
-      # libllvm
-      sd
-      direnv
-      amazon-q-cli
-      zellij
-      codex
-      nil
-      brave
-    ]
-    ++ (
-      if arch == "aarch64"
-      then []
-      else []
-    )
-    ++ (
-      if arch == "x86_64"
-      then []
-      else []
-    )
-    ++ (
-      if os == "darwin"
-      then [
+  [
+    fenix.packages.${system}.latest.toolchain
+    git
+    curl
+    dprint
+    eza
+    fd
+    gh
+    gitui
+    lua5_4_compat
+    lua-language-server
+    luajitPackages.luarocks
+    marksman
+    markdown-oxide
+    neovim
+    ripgrep
+    rm-improved
+    skim
+    starship
+    stylua
+    taplo
+    zoxide
+    nerd-fonts.meslo-lg
+    # alejandra
+    obsidian
+    zsh
+    tailscale
+    bat
+    tokei
+    cargo-expand
+    yaml-language-server
+    # sbarlua build dependencies
+    # clang
+    # libllvm
+    sd
+    direnv
+    amazon-q-cli
+    zellij
+    codex
+    nil
+    brave
+  ]
+  ++ (if arch == "aarch64" then [ ] else [ ])
+  ++ (if arch == "x86_64" then [ ] else [ ])
+  ++ (
+    if os == "darwin" then
+      [
         chatgpt
         mas
         pngpaste
@@ -75,19 +68,20 @@
         ghostty-bin
         anki-bin
       ]
-      else []
-    )
-    ++ (
-      if os == "linux"
-      then [
+    else
+      [ ]
+  )
+  ++ (
+    if os == "linux" then
+      [
         anki
         unixtools.xxd
         firefox
         docker
         ghostty
         swift
-        # niri
-		  hyprland
+        niri
+        # hyprland
         regreet
         ironbar
         sherlock
@@ -98,45 +92,23 @@
         swww
         # clipcat
       ]
-      else []
-    )
-    ++ (
-      if arch == "aarch64" && os == "darwin"
-      then []
-      else []
-    )
-    ++ (
-      if arch == "x86_64" && os == "darwin"
-      then []
-      else []
-    )
-    ++ (
-      if arch == "aarch64" && os == "linux"
-      then [slacky]
-      else []
-    )
-    ++ (
-      if arch == "x86_64" && os == "linux"
-      then [
+    else
+      [ ]
+  )
+  ++ (if arch == "aarch64" && os == "darwin" then [ ] else [ ])
+  ++ (if arch == "x86_64" && os == "darwin" then [ ] else [ ])
+  ++ (if arch == "aarch64" && os == "linux" then [ slacky ] else [ ])
+  ++ (
+    if arch == "x86_64" && os == "linux" then
+      [
         discord
         slack
         vital
       ]
-      else []
-    )
-    ++ (
-      if user == "a"
-      then []
-      else []
-    )
-    ++ (
-      if user == "hiromichi-sugiura"
-      then []
-      else []
-    )
-    ++ (
-      if user == "xsugiurah"
-      then []
-      else []
-    )
+    else
+      [ ]
+  )
+  ++ (if user == "a" then [ ] else [ ])
+  ++ (if user == "hiromichi-sugiura" then [ ] else [ ])
+  ++ (if user == "xsugiurah" then [ ] else [ ])
 )
